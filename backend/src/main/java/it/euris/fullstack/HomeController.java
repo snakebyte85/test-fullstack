d976da28d58e92438ab17f286602184b8366a778
Valerio <snakebyte8@gmail.com>
2017-06-01 12:33:04 +0200

    Primo commit

---
+++ backend/src/main/java/it/euris/fullstack/HomeController.java
@@ -0,0 +1,18 @@
+package it.euris.fullstack;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    String home() {
+        return "TEST";
+    }
+
+    
+}
---
+++ backend/src/main/java/it/euris/fullstack/MainApplication.java
@@ -0,0 +1,28 @@
+package it.euris.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class MainApplication {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(MainApplication.class, args);
+  }
+
+  @Bean
+  WebMvcConfigurer configurer() {
+    return new WebMvcConfigurerAdapter() {
+      
+      @Override
+public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("forward:/index.html");
+}
+ 
+  };
+  }
+}
