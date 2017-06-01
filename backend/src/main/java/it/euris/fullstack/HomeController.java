6263e3e78f1fde89f4962721df46b569ed77e41f
Valerio <snakebyte8@gmail.com>
2017-06-01 12:39:42 +0200

    Formattazione

--- backend/src/main/java/it/euris/fullstack/HomeController.java
+++ backend/src/main/java/it/euris/fullstack/HomeController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class HomeController {
 
     @RequestMapping("/test")
     @ResponseBody
     String home() {
-        return "TEST";
+	return "TEST";
     }
 
-    
 }
--- backend/src/main/java/it/euris/fullstack/MainApplication.java
+++ backend/src/main/java/it/euris/fullstack/MainApplication.java
@@ -10,19 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class MainApplication {
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(MainApplication.class, args);
-  }
+    public static void main(String[] args) throws Exception {
+	SpringApplication.run(MainApplication.class, args);
+    }
 
-  @Bean
-  WebMvcConfigurer configurer() {
-    return new WebMvcConfigurerAdapter() {
-      
-      @Override
-public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/").setViewName("forward:/index.html");
-}
- 
-  };
-  }
+    @Bean
+    WebMvcConfigurer configurer() {
+	return new WebMvcConfigurerAdapter() {
+
+	    @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+	    }
+
+	};
+    }
 }
